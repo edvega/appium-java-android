e258340f935f0efb74e786bf3b1ff59606af30f9
edvega <edwega@gmail.com>
2020-02-18 22:44:12 -0600

    Swipe event

---
+++ src/SwipeDemo.java
@@ -0,0 +1,18 @@
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class SwipeDemo extends Base {
+
+	public static void main(String[] args) throws MalformedURLException {
+		
+		AndroidDriver<AndroidElement> driver = Base.capabilities();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElementByXPath("//android.widget.TextView[@text='Views']").click();
+		driver.findElementByXPath("//android.widget.TextView[@text='Date Widgets']").click();
+		driver.findElementByAndroidUIAutomator("text(\"2. Inline\")").click();
+		driver.findElementByXPath("//*[@content-desc='9']").click();
+	}
+}
