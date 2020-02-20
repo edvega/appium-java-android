6c8ddc0a0d2c9384711b7b8c2c0f2e34c29cbfda
edvega <edwega@gmail.com>
2020-02-19 20:33:25 -0600

    Scrolling and Drag & Drop demo

---
+++ src/DragAndDropDemo.java
@@ -0,0 +1,27 @@
+import static io.appium.java_client.touch.offset.ElementOption.element;
+
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebElement;
+
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class DragAndDropDemo extends Base {
+
+	public static void main(String[] args) throws MalformedURLException {
+		
+		AndroidDriver<AndroidElement> driver = Base.capabilities();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.findElementByXPath("//android.widget.TextView[@text='Views']").click();
+		driver.findElementByXPath("//android.widget.TextView[@text='Drag and Drop']").click();
+		WebElement source = driver.findElementsByClassName("android.view.View").get(0);
+		WebElement destination = driver.findElementsByClassName("android.view.View").get(1);
+		
+		TouchAction t = new TouchAction(driver);
+		t.longPress(element(source)).moveTo(element(destination)).release().perform();
+	}
+}
---
+++ src/ScrollingDemo.java
@@ -0,0 +1,17 @@
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class ScrollingDemo extends Base {
+
+	public static void main(String[] args) throws MalformedURLException {
+		
+		AndroidDriver<AndroidElement> driver = Base.capabilities();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.findElementByXPath("//android.widget.TextView[@text='Views']").click();
+		driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"WebView\"));");
+	}
+}
