7ef7b9b6b3c772a29929831baac5ea6643a8f909
edvega <edwega@gmail.com>
2020-02-19 19:28:17 -0600

    Swipe demo

--- src/SwipeDemo.java
+++ src/SwipeDemo.java
@@ -1,6 +1,12 @@
 import java.net.MalformedURLException;
 import java.util.concurrent.TimeUnit;
+import static java.time.Duration.ofSeconds;
 
+import org.openqa.selenium.WebElement;
+
+import io.appium.java_client.TouchAction;
+import static io.appium.java_client.touch.LongPressOptions.longPressOptions;
+import static io.appium.java_client.touch.offset.ElementOption.element;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 
@@ -14,5 +20,11 @@ public class SwipeDemo extends Base {
 		driver.findElementByXPath("//android.widget.TextView[@text='Date Widgets']").click();
 		driver.findElementByAndroidUIAutomator("text(\"2. Inline\")").click();
 		driver.findElementByXPath("//*[@content-desc='9']").click();
+		
+		TouchAction t = new TouchAction(driver);
+		WebElement first = driver.findElementByXPath("//*[@content-desc='15']");
+		WebElement second = driver.findElementByXPath("//*[@content-desc='45']");
+		t.longPress(longPressOptions().withElement(element(first)).withDuration(ofSeconds(2)))
+					.moveTo(element(second)).release().perform();
 	}
 }
